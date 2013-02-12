f53438a91eb8353a7bda088ac1baa1698fa79bc0
Frank Purcell <fxpurcell@gmail.com>
2013-02-11 18:44:15 -0800

    fix up geojson stuff...

--- ott/carshare/service.py
+++ ott/carshare/service.py
@@ -7,13 +7,13 @@ def main():
     args = init_parser()
     print args
     db = Database(args.url, args.schema)
-    
-    
+
     f1 = geojson.Feature(id=1, geometry=geojson.Point(coordinates=(53.04781795911469, -4.10888671875)))
     f2 = geojson.Feature(id=2, geometry=geojson.Point(coordinates=(53.04781795911469, -4.10888671875)))
     f  = [f1, f2]
     fc = geojson.FeatureCollection(f)
-    print fc
+    json = geojson.dumps(fc, sort_keys=True)
+    print json
 
 if __name__ == '__main__':
     main()
