c162fb97b321aad90bfa3422ca8cf5176287ab50
Frank Purcell <fxpurcell@gmail.com>
2014-04-27 13:56:26 -0700

    clean up imports

--- ott/carshare/model/vehicle.py
+++ ott/carshare/model/vehicle.py
@@ -4,6 +4,7 @@ from sqlalchemy import Column, Index, Integer, Numeric, String, DateTime
 from sqlalchemy.sql import func, and_
 
 from ott.carshare.model.base import Base
+from ott.carshare.model.position import Position
 
 class Vehicle(Base):
     __tablename__ = 'vehicles'
@@ -32,7 +33,6 @@ class Vehicle(Base):
             same place as an earlier update, update the 
             NOTE: the position add/update needs to be committed to the db by the caller of this method 
         '''
-        from ott.carshare.model.position import Position
 
         # step 1: get position object from db ...criteria is to find last position 
         #          update within an hour, and the car hasn't moved lat,lon
